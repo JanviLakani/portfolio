76bdcd150b122658207bb466e30abf8a9017bc3b
JanviLakani <lakanijanvi1999@gmail.com>
2025-09-25 09:42:26 +0530

    add 24_09 refresh theme toogel useEffect use

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,19 +1,58 @@
-import { Children, createContext, useReducer } from "react";
+// import { Children, createContext, useReducer } from "react";
+// import { themeReducer } from "./Reducer/Theme.reducer";
+// import { THEME_TYPE } from "./ActionTypes";
+
+// const initialState = {
+//   theme: "light",
+// };
+
+// export const ThemeContext = createContext();
+
+// export const ThemeProvider = ({children}) => {
+//   const [state, dispatch] = useReducer(themeReducer, initialState);
+
+// const toogleTheme = (val) => {
+//     dispatch({ type: THEME_TYPE, payload: val === "light" ? "dark" : "light" });
+//   };
+
+//   return (
+//     <ThemeContext.Provider value={{ ...state, toogleTheme }}>
+//       {children}
+//     </ThemeContext.Provider>
+//   );
+// };
+
+import { Children, createContext, useEffect, useReducer } from "react";
 import { themeReducer } from "./Reducer/Theme.reducer";
 import { THEME_TYPE } from "./ActionTypes";
 
-const initialState = {
-  theme: "light",
+// const initialState = {
+//   theme: "light",
+// };
+
+const initialState = () => {
+  const saveTheme = localStorage.getItem("theme");
 };
 
 export const ThemeContext = createContext();
 
-export const ThemeProvider = ({children}) => {
+export const ThemeProvider = ({ children }) => {
   const [state, dispatch] = useReducer(themeReducer, initialState);
 
-const toogleTheme = (val) => {
+  const toogleTheme = (val) => {
+    const newTheme = val === "light" ? "dark" : "light";
+    localStorage.setItem("theme", newTheme);
+    console.log("newTheme", newTheme);
+
     dispatch({ type: THEME_TYPE, payload: val === "light" ? "dark" : "light" });
   };
+
+  useEffect(() => {
+    const saveTheme = localStorage.getItem("theme");
+      console.log("saveTheme",  saveTheme);
+
+    
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ ...state, toogleTheme }}>
